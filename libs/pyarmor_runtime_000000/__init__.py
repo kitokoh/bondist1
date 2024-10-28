326697696ac40e4bc45b5ba4de385fb62ef561b2
kitokoh <ibrahimkoubaye@outlook.com>
2024-10-28 19:52:55 +0300

    ok

--- libs/pyarmor_runtime_000000/__init__.py
+++ libs/pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.0.3 (trial), 000000, 2024-10-28T19:48:13.556497
+# Pyarmor 9.0.3 (trial), 000000, 2024-10-28T19:52:11.592127
 from .pyarmor_runtime import __pyarmor__
--- pyarmor_runtime_000000/__init__.py
+++ pyarmor_runtime_000000/__init__.py
@@ -1,2 +1,2 @@
-# Pyarmor 9.0.3 (trial), 000000, 2024-10-28T19:48:17.692700
+# Pyarmor 9.0.3 (trial), 000000, 2024-10-28T19:52:13.996480
 from .pyarmor_runtime import __pyarmor__
